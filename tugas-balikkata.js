8e0d3afcd32e8576b26b147e736f7950b08dce40
Mukhamat Jafar <jafar.mukhamat7@gmail.com>
2019-08-18 23:04:15 +0700

    tugas_balik_kata_js
    tugas sudah ok !!!

---
+++ tugas-balikkata.js
@@ -0,0 +1,18 @@
+ function panggilbalikata(kata){
+
+   return kata.split('').reverse().join('');
+
+}
+console.log(panggilbalikata("Niomic!"));
+console.log(panggilbalikata("Javascript"));
+console.log(panggilbalikata("alohaaloha"));
+console.log(panggilbalikata("Jawa Barat"));
+
+
+// var i = 0;
+//
+//
+// while (i<10) {
+//   console.log("ini urutan ke ", i);
+//   ++i;
+// }
